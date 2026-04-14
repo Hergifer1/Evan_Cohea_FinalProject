bfc391f92fd8a4a9f1f495d0d777ce6bf0189193
Evan Cohea <dal651233@UTDallas.edu>
2026-04-14 18:04:50 -0500

    Program creates window

--- src/Dino.py
+++ src/Dino.py
@@ -1,18 +1,25 @@
 import pygame
 import random
 
-
 def main():
     pygame.init()
     pygame.display.set_caption("Dinosaur Game")
     clock = pygame.time.Clock()
     dt = 0
-    screen = pygame.display.set_mode((500, 600))
+    screen = pygame.display.set_mode((800, 500))
     running = True
+
+    #Create character
+    Dino = pygame.image.load('Dino_Standing.png')
     while running:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
+        screen.fill((0, 0, 0))
+        
         pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
